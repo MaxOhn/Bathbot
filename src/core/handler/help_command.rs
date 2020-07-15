d34afca2704353ccb9134c71993baef68c770b4f
MaxOhn <ohn.m@hotmail.de>
2020-07-15 19:16:38 +0200

    Finished help_command

--- src/core/handler/help_command.rs
+++ src/core/handler/help_command.rs
@@ -8,5 +8,37 @@ use std::fmt::Write;
 use twilight::{builders::embed::EmbedBuilder, model::channel::Message};
 
 pub async fn help_command(ctx: &Context, cmd: &Command, msg: &Message) -> BotResult<()> {
-    todo!()
+    msg.build_response(ctx, |m| {
+        let name = cmd.names[0];
+        let mut eb = EmbedBuilder::new().color(DARK_GREEN).title(name);
+        if let Some(description) = cmd.long_desc {
+            eb = eb.description(description);
+        }
+        if let Some(usage) = cmd.usage {
+            eb = eb.add_field("How to use", usage).inline().commit();
+        }
+        if !cmd.examples.is_empty() {
+            let len: usize = cmd.examples.iter().map(|e| name.len() + e.len() + 4).sum();
+            let mut value = String::with_capacity(len);
+            let mut examples = cmd.examples.iter();
+            writeln!(value, "`{} {}`", name, examples.next().unwrap());
+            for example in examples {
+                writeln!(value, "`{} {}`", name, example);
+            }
+            eb = eb.add_field("Examples", value).inline().commit();
+        }
+        if cmd.names.len() > 1 {
+            let len: usize = cmd.names.iter().skip(1).map(|n| 4 + n.len()).sum();
+            let mut value = String::with_capacity(len);
+            let mut aliases = cmd.names.iter().skip(1);
+            write!(value, "`{}`", aliases.next().unwrap());
+            for alias in aliases {
+                write!(value, ", `{}`", alias);
+            }
+            eb = eb.add_field("Aliases", value).inline().commit();
+        }
+        m.embed(eb.build())
+    })
+    .await?;
+    Ok(())
 }
