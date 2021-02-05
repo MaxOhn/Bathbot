62716de9b267599fc2d4d0a22494b7c211589990
MaxOhn <ohn.m@hotmail.de>
2021-02-05 16:25:58 +0100

    Made 'track' commands numbered

--- src/commands/tracking/track.rs
+++ src/commands/tracking/track.rs
@@ -16,25 +16,31 @@ async fn track_main(
     ctx: Arc<Context>,
     msg: &Message,
     args: Args<'_>,
+    num: Option<usize>,
 ) -> BotResult<()> {
     let args = match MultNameLimitArgs::new(&ctx, args, 10) {
         Ok(args) => args,
         Err(err_msg) => return msg.error(&ctx, err_msg).await,
     };
+
     let names = if args.names.is_empty() {
         let content = "You need to specify at least one osu username";
+
         return msg.error(&ctx, content).await;
     } else {
         args.names.into_iter().collect::<HashSet<_>>()
     };
+
     if let Some(name) = names.iter().find(|name| name.len() > 15) {
         let content = format!("`{}` is too long for an osu! username", name);
+
         return msg.error(&ctx, content).await;
     }
 
-    let limit = match args.limit {
+    let limit = match args.limit.or(num) {
         Some(limit) if limit == 0 || limit > 100 => {
             let content = "The given limit must be between 1 and 100";
+
             return msg.error(&ctx, content).await;
         }
         Some(limit) => limit,
@@ -48,10 +54,12 @@ async fn track_main(
             .mode(mode)
             .map_ok(move |user| (name, user))
     });
+
     let users: Vec<(u32, String)> = match try_join_all(user_futs).await {
         Ok(users) => match users.iter().find(|(_, user)| user.is_none()) {
             Some((name, _)) => {
                 let content = format!("User `{}` was not found", name);
+
                 return msg.error(&ctx, content).await;
             }
             None => users
@@ -61,6 +69,7 @@ async fn track_main(
         },
         Err(why) => {
             let _ = msg.error(&ctx, OSU_API_ISSUE).await;
+
             return Err(why.into());
         }
     };
@@ -68,21 +77,26 @@ async fn track_main(
         let content = "None of the given users were found by the API";
         return msg.error(&ctx, content).await;
     }
+
     let channel = msg.channel_id;
     let mut success = Vec::with_capacity(users.len());
     let mut failure = Vec::new();
+
     for (user_id, username) in users {
         let add_fut = ctx
             .tracking()
             .add(user_id, mode, Utc::now(), channel, limit, ctx.psql());
+
         match add_fut.await {
             Ok(true) => success.push(username),
             Ok(false) => failure.push(username),
             Err(why) => {
                 warn!("Error while adding tracked entry: {}", why);
+
                 let embed = TrackEmbed::new(mode, success, failure, Some(username), limit)
                     .build_owned()
                     .build()?;
+
                 return msg.build_response(&ctx, |m| m.embed(embed)).await;
             }
         }
@@ -90,7 +104,9 @@ async fn track_main(
     let embed = TrackEmbed::new(mode, success, failure, None, limit)
         .build_owned()
         .build()?;
+
     msg.build_response(&ctx, |m| m.embed(embed)).await?;
+
     Ok(())
 }
 
@@ -98,11 +114,15 @@ async fn track_main(
 #[authority()]
 #[short_desc("Track osu!standard user(s') top scores")]
 #[long_desc(
-    "Track osu! standard user(s') top scores and notify a channel \
+    "Track osu!standard user(s') top scores and notify a channel \
     about new plays in their top100.\n\
-    You can specify up to ten usernames per command invokation.\n\
-    To provide a limit, specify `-limit` followed by a number \
-    between 1 and 100, defaults to 100."
+    You can specify up to ten usernames per command invocation.\n\
+    To provide a limit, specify a number right after the command, \
+    e.g. `track42 badewanne3` to only notify if `badewanne3` got \
+    a new score in his top 42.\n\
+    Alternatively, you can provide a limit by specifying `-limit` \
+    followed by a number, e.g. `track -limit 42 badewanne3`.\n\
+    The limit must be between 1 and 100, defaults to 100 if none is given."
 )]
 #[usage("[-limit number] [username1] [username2] ...")]
 #[example(
@@ -110,8 +130,13 @@ async fn track_main(
     "-limit 45 cookiezi whitecat",
     "\"freddie benson\""
 )]
-pub async fn track(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResult<()> {
-    track_main(GameMode::STD, ctx, msg, args).await
+pub async fn track(
+    ctx: Arc<Context>,
+    msg: &Message,
+    args: Args,
+    num: Option<usize>,
+) -> BotResult<()> {
+    track_main(GameMode::STD, ctx, msg, args, num).await
 }
 
 #[command]
@@ -120,9 +145,13 @@ pub async fn track(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResult<()
 #[long_desc(
     "Track mania user(s') top scores and notify a channel \
     about new plays in their top100.\n\
-    You can specify up to ten usernames per command invokation.\n\
-    To provide a limit, specify `-limit` followed by a number \
-    between 1 and 100, defaults to 100."
+    You can specify __up to ten usernames__ per command invocation.\n\
+    To provide a limit, specify a number right after the command, \
+    e.g. `trackmania42 badewanne3` to only notify if `badewanne3` got \
+    a new score in his top 42.\n\
+    Alternatively, you can provide a limit by specifying `-limit` \
+    followed by a number, e.g. `trackmania -limit 42 badewanne3`.\n\
+    The limit must be between 1 and 100, defaults to 100 if none is given."
 )]
 #[usage("[-limit number] [username1] [username2] ...")]
 #[example(
@@ -130,8 +159,13 @@ pub async fn track(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResult<()
     "-limit 45 cookiezi whitecat",
     "\"freddie benson\""
 )]
-pub async fn trackmania(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResult<()> {
-    track_main(GameMode::MNA, ctx, msg, args).await
+pub async fn trackmania(
+    ctx: Arc<Context>,
+    msg: &Message,
+    args: Args,
+    num: Option<usize>,
+) -> BotResult<()> {
+    track_main(GameMode::MNA, ctx, msg, args, num).await
 }
 
 #[command]
@@ -140,9 +174,13 @@ pub async fn trackmania(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResu
 #[long_desc(
     "Track taiko user(s') top scores and notify a channel \
     about new plays in their top100.\n\
-    You can specify up to ten usernames per command invokation.\n\
-    To provide a limit, specify `-limit` followed by a number \
-    between 1 and 100, defaults to 100."
+    You can specify __up to ten usernames__ per command invocation.\n\
+    To provide a limit, specify a number right after the command, \
+    e.g. `tracktaiko42 badewanne3` to only notify if `badewanne3` got \
+    a new score in his top 42.\n\
+    Alternatively, you can provide a limit by specifying `-limit` \
+    followed by a number, e.g. `tracktaiko -limit 42 badewanne3`.\n\
+    The limit must be between 1 and 100, defaults to 100 if none is given."
 )]
 #[usage("[-limit number] [username1] [username2] ...")]
 #[example(
@@ -150,8 +188,13 @@ pub async fn trackmania(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResu
     "-limit 45 cookiezi whitecat",
     "\"freddie benson\""
 )]
-pub async fn tracktaiko(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResult<()> {
-    track_main(GameMode::TKO, ctx, msg, args).await
+pub async fn tracktaiko(
+    ctx: Arc<Context>,
+    msg: &Message,
+    args: Args,
+    num: Option<usize>,
+) -> BotResult<()> {
+    track_main(GameMode::TKO, ctx, msg, args, num).await
 }
 
 #[command]
@@ -160,9 +203,13 @@ pub async fn tracktaiko(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResu
 #[long_desc(
     "Track ctb user(s') top scores and notify a channel \
     about new plays in their top100.\n\
-    You can specify up to ten usernames per command invokation.\n\
-    To provide a limit, specify `-limit` followed by a number \
-    between 1 and 100, defaults to 100."
+    You can specify __up to ten usernames__ per command invocation.\n\
+    To provide a limit, specify a number right after the command, \
+    e.g. `trackctb42 badewanne3` to only notify if `badewanne3` got \
+    a new score in his top 42.\n\
+    Alternatively, you can provide a limit by specifying `-limit` \
+    followed by a number, e.g. `trackctb -limit 42 badewanne3`.\n\
+    The limit must be between 1 and 100, defaults to 100 if none is given."
 )]
 #[usage("[-limit number] [username1] [username2] ...")]
 #[example(
@@ -170,6 +217,11 @@ pub async fn tracktaiko(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResu
     "-limit 45 cookiezi whitecat",
     "\"freddie benson\""
 )]
-pub async fn trackctb(ctx: Arc<Context>, msg: &Message, args: Args) -> BotResult<()> {
-    track_main(GameMode::CTB, ctx, msg, args).await
+pub async fn trackctb(
+    ctx: Arc<Context>,
+    msg: &Message,
+    args: Args,
+    num: Option<usize>,
+) -> BotResult<()> {
+    track_main(GameMode::CTB, ctx, msg, args, num).await
 }
