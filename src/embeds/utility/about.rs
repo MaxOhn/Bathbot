4f61ab1f8c98aa431d3e1da51d458d18ef10e821
MaxOhn <ohn.m@hotmail.de>
2021-06-30 16:50:07 +0200

    revamp <about & remove sysinfo dep

--- src/embeds/utility/about.rs
+++ src/embeds/utility/about.rs
@@ -1,59 +1,35 @@
 use crate::{
     bail,
     embeds::{EmbedFields, Footer},
-    format_err,
     util::{
         constants::{BATHBOT_WORKSHOP, INVITE_LINK, OWNER_USER_ID},
+        datetime::how_long_ago,
         discord_avatar,
         numbers::with_comma_uint,
     },
     BotResult, Context,
 };
 
-use chrono::{DateTime, Utc};
-use sysinfo::{get_current_pid, ProcessExt, ProcessorExt, RefreshKind, System, SystemExt};
+use prometheus::core::Collector;
 use twilight_model::id::UserId;
 
 pub struct AboutEmbed {
     fields: EmbedFields,
     footer: Footer,
     thumbnail: String,
-    timestamp: DateTime<Utc>,
     title: String,
 }
 
 impl AboutEmbed {
     pub async fn new(ctx: &Context) -> BotResult<Self> {
-        let owner = ctx
-            .http
-            .user(UserId(OWNER_USER_ID))
-            .await?
-            .ok_or_else(|| format_err!("Cache does not contain user of owner"))?;
+        let owner_id = UserId(OWNER_USER_ID);
 
-        let refreshes = RefreshKind::new().with_cpu().with_memory().with_processes();
-        let mut system = System::new_with_specifics(refreshes);
-
-        let (process_ram, total_cpu, used_ram, total_ram) = {
-            system.refresh_specifics(refreshes);
-
-            let pid = get_current_pid()
-                .map_err(|why| format_err!("Could not get current PID: {}", why))?;
-
-            let process = system
-                .process(pid)
-                .ok_or_else(|| format_err!("No process with PID {}", pid))?;
-
-            let process_ram = process.memory() / 1000;
-            let processors = system.processors();
-            let processor_count = processors.len() as f32;
-
-            let total_cpu: f32 =
-                processors.iter().map(ProcessorExt::cpu_usage).sum::<f32>() / processor_count;
-
-            let used_ram = (system.used_memory() + system.used_swap()) / 1000;
-            let total_ram = (system.total_memory() + system.total_swap()) / 1000;
-
-            (process_ram, total_cpu, used_ram, total_ram)
+        let owner = match ctx.cache.user(owner_id) {
+            Some(user) => user,
+            None => match ctx.http.user(owner_id).await? {
+                Some(user) => user,
+                None => bail!("User of owner could not be retrieved"),
+            },
         };
 
         let bot_user = match ctx.cache.current_user() {
@@ -61,40 +37,55 @@ impl AboutEmbed {
             None => bail!("No CurrentUser in cache"),
         };
 
-        let name = bot_user.name.clone();
+        let name = bot_user.name;
         let shards = ctx.backend.cluster.info().len();
         let guilds = ctx.stats.cache_metrics.guilds.get();
-
         let boot_time = ctx.stats.start_time;
+
+        let commands_used: usize = ctx.stats.command_counts.collect()[0]
+            .get_metric()
+            .iter()
+            .map(|metrics| metrics.get_counter().get_value() as usize)
+            .sum();
+
+        let osu_requests: usize = ctx.stats.osu_metrics.rosu.collect()[0]
+            .get_metric()
+            .iter()
+            .map(|metric| metric.get_counter().get_value() as usize)
+            .sum();
 
         let thumbnail = discord_avatar(bot_user.id, bot_user.avatar.as_deref().unwrap());
 
-        let footer = Footer::new(format!(
-            "Owner: {}#{} | Boot time",
-            owner.name, owner.discriminator
-        ))
-        .icon_url(discord_avatar(owner.id, owner.avatar.as_deref().unwrap()));
+        let footer = Footer::new(format!("Owner: {}#{}", owner.name, owner.discriminator))
+            .icon_url(discord_avatar(owner.id, owner.avatar.as_deref().unwrap()));
 
         let fields = vec![
             field!("Guilds", with_comma_uint(guilds as u64).to_string(), true),
-            field!("Total RAM", format!("{}/{} MB", used_ram, total_ram), true),
-            field!("Process RAM", format!("{} MB", process_ram), true),
             field!("Shards", shards.to_string(), true),
-            field!("Total CPU", format!("{:.2}%", total_cpu), true),
+            field!("Boot-up", how_long_ago(&boot_time).to_string(), true),
+            field!(
+                "Commands used",
+                with_comma_uint(commands_used).to_string(),
+                true
+            ),
+            field!(
+                "osu!api requests",
+                with_comma_uint(osu_requests).to_string(),
+                true
+            ),
+            field!("Invite link", INVITE_LINK.to_owned(), false),
+            field!("Bathbot discord server", BATHBOT_WORKSHOP.to_owned(), false),
             field!(
                 "Github",
                 "https://github.com/MaxOhn/Bathbot".to_string(),
                 false
             ),
-            field!("Invite link", INVITE_LINK.to_owned(), false),
-            field!("Bathbot discord server", BATHBOT_WORKSHOP.to_owned(), false),
         ];
 
         Ok(Self {
             fields,
             footer,
             thumbnail,
-            timestamp: boot_time,
             title: format!("About {}", name),
         })
     }
@@ -104,6 +95,5 @@ impl_builder!(AboutEmbed {
     fields,
     footer,
     thumbnail,
-    timestamp,
     title,
 });
