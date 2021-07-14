b85e26ce599f41d0f369b74e0fafd94ee8fdbbda
MaxOhn <ohn.m@hotmail.de>
2021-07-14 15:22:20 +0200

    dont multithread background loop

--- src/core/context/impls/background_loop.rs
+++ src/core/context/impls/background_loop.rs
@@ -8,7 +8,7 @@ use rosu_v2::prelude::{
 use std::sync::Arc;
 use tokio::{
     fs::remove_file,
-    time::{interval, Duration},
+    time::{self, Duration},
 };
 
 impl Context {
@@ -18,21 +18,32 @@ impl Context {
     }
 
     pub async fn garbage_collect_all_maps(&self) -> usize {
-        let mut garbage_collection = self.data.map_garbage_collection.lock().await;
+        let five_seconds = Duration::from_secs(5);
+
+        let mut garbage_collection =
+            match time::timeout(five_seconds, self.data.map_garbage_collection.lock()).await {
+                Ok(guard) => guard,
+                Err(_) => {
+                    warn!("Failed to acquire lock for garbage collection");
+
+                    return 0;
+                }
+            };
 
         if garbage_collection.is_empty() {
             return 0;
         }
 
         let config = CONFIG.get().unwrap();
+        let total = garbage_collection.len();
 
         let tasks = garbage_collection.drain().map(|map_id| async move {
             let mut map_path = config.map_path.clone();
             map_path.push(format!("{}.osu", map_id));
 
-            match remove_file(map_path).await {
-                Ok(_) => None,
-                Err(_) => Some(map_id),
+            match time::timeout(five_seconds, remove_file(map_path)).await {
+                Ok(Ok(_)) => None,
+                Ok(Err(_)) | Err(_) => Some(map_id),
             }
         });
 
@@ -52,8 +63,9 @@ impl Context {
 
         if !failed.is_empty() {
             warn!(
-                "Failed to garbage collect {} maps: {:?}",
+                "Failed to garbage collect {}/{} maps: {:?}",
                 failed.len(),
+                total,
                 failed,
             );
         }
@@ -73,7 +85,7 @@ impl Context {
         }
 
         // Once per day
-        let mut interval = interval(Duration::from_secs(60 * 60 * 24));
+        let mut interval = time::interval(Duration::from_secs(60 * 60 * 24));
         interval.tick().await;
 
         loop {
@@ -81,18 +93,14 @@ impl Context {
 
             debug!("[BG] Background iteration...");
 
-            let (count, guild_res) = tokio::join!(
-                ctx.garbage_collect_all_maps(),
-                ctx.psql().insert_guilds(&ctx.data.guilds)
-            );
-
-            debug!("[BG] Garbage collected {} maps", count);
-
-            match guild_res {
+            match ctx.psql().insert_guilds(&ctx.data.guilds).await {
                 Ok(0) => debug!("[BG] No new or modified guilds to store in DB"),
                 Ok(n) => debug!("[BG] Stored {} guilds in DB", n),
                 Err(why) => warn!("[BG] Error while storing guilds in DB: {}", why),
             }
+
+            let count = ctx.garbage_collect_all_maps().await;
+            debug!("[BG] Garbage collected {} maps", count);
         }
     }
 }
