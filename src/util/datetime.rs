4c7d9cb5290dd86f3028b3c2c1ab44cea397c869
MaxOhn <ohn.m@hotmail.de>
2021-08-11 22:56:06 +0200

    use discords dynamic timestamp formatting

--- src/util/datetime.rs
+++ src/util/datetime.rs
@@ -1,7 +1,7 @@
 use super::constants::DATE_FORMAT;
 use crate::BotResult;
 
-use chrono::{offset::TimeZone, DateTime, Datelike, Utc};
+use chrono::{offset::TimeZone, DateTime, Utc};
 use std::fmt;
 
 pub fn date_to_string(date: &DateTime<Utc>) -> String {
@@ -27,57 +27,16 @@ impl fmt::Display for SecToMinSecFormatter {
     }
 }
 
-// thx saki :)
 pub fn how_long_ago(date: &DateTime<Utc>) -> HowLongAgoFormatter {
-    HowLongAgoFormatter(date)
+    HowLongAgoFormatter(date.timestamp())
 }
 
-pub struct HowLongAgoFormatter<'a>(&'a DateTime<Utc>);
+pub struct HowLongAgoFormatter(i64);
 
-impl<'a> fmt::Display for HowLongAgoFormatter<'a> {
+impl fmt::Display for HowLongAgoFormatter {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let now = Utc::now();
-        let diff_sec = now.timestamp() - self.0.timestamp();
-
-        debug_assert!(diff_sec >= 0);
-
-        let one_day = 24 * 3600;
-        let one_week = 7 * one_day;
-
-        let (amount, unit) = {
-            if diff_sec < 60 {
-                (diff_sec, "second")
-            } else if diff_sec < 3600 {
-                (diff_sec / 60, "minute")
-            } else if diff_sec < one_day {
-                (diff_sec / 3600, "hour")
-            } else if diff_sec < one_week {
-                (diff_sec / one_day, "day")
-            } else if diff_sec < 4 * one_week {
-                (diff_sec / one_week, "week")
-            } else {
-                let diff_month = (12 * (now.year() - self.0.year()) as u32 + now.month()
-                    - self.0.month()) as i64;
-
-                if diff_month < 1 {
-                    (diff_sec / one_week, "week")
-                } else if diff_month < 12 {
-                    (diff_month, "month")
-                } else {
-                    let years = diff_month / 12 + (diff_month % 12 > 9) as i64;
-
-                    (years, "year")
-                }
-            }
-        };
-
-        write!(
-            f,
-            "{amount} {unit}{plural} ago",
-            amount = amount,
-            unit = unit,
-            plural = if amount == 1 { "" } else { "s" }
-        )
+        // https://discord.com/developers/docs/reference#message-formatting-timestamp-styles
+        write!(f, "<t:{}:R>", self.0)
     }
 }
 
