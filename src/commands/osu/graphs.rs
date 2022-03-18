72e42ad04b56f0b311b72aa918509440e8985e39
MaxOhn <ohn.m@hotmail.de>
2022-03-18 03:57:42 +0100

    finished rank subcommand of /graph

--- src/commands/osu/graphs.rs
+++ src/commands/osu/graphs.rs
@@ -5,9 +5,10 @@ use eyre::Report;
 use image::{png::PngEncoder, ColorType, ImageEncoder};
 use plotters::{
     prelude::{
-        BitMapBackend, ChartBuilder, IntoDrawingArea, IntoSegmentedCoord, Rectangle, SegmentValue,
+        AreaSeries, BitMapBackend, ChartBuilder, Circle, IntoDrawingArea, IntoSegmentedCoord,
+        Rectangle, SegmentValue, SeriesLabelPosition,
     },
-    style::{Color, RGBColor, WHITE},
+    style::{Color, RGBColor, ShapeStyle, BLACK, GREEN, RED, WHITE},
 };
 use plotters_backend::FontStyle;
 use rosu_v2::prelude::{GameMode, OsuError, Score, User};
@@ -76,7 +77,7 @@ async fn graph(ctx: Arc<Context>, data: CommandData<'_>, args: GraphArgs) -> Bot
                     national = stats.country_rank.unwrap_or(0)
                 );
 
-                let url = format!("{OSU_BASE}users/{}/{}", user.user_id, user.mode,);
+                let url = format!("{OSU_BASE}users/{}/{}", user.user_id, user.mode);
                 let icon = flag_url(user.country_code.as_str());
 
                 Author::new(text).url(url).icon_url(icon)
@@ -203,7 +204,7 @@ async fn rank_graph(
     name: &str,
     user_args: &UserArgs<'_>,
 ) -> BotResult<Option<(User, Vec<u8>)>> {
-    let _user = match get_user(ctx, &user_args).await {
+    let user = match get_user(ctx, &user_args).await {
         Ok(user) => user,
         Err(OsuError::NotFound) => {
             let content = format!("Could not find user `{name}`");
@@ -218,7 +219,151 @@ async fn rank_graph(
         }
     };
 
-    todo!()
+    fn draw_graph(user: &User) -> Result<Option<Vec<u8>>, GraphError> {
+        const W: u32 = 750;
+        const H: u32 = 400;
+        const LEN: usize = (W * H) as usize * 3;
+
+        let mut buf = vec![0; LEN];
+
+        let history = match user.rank_history {
+            Some(ref history) if history.is_empty() => return Ok(None),
+            Some(ref history) => history,
+            None => return Ok(None),
+        };
+
+        let history_len = history.len();
+
+        let mut min = u32::MAX;
+        let mut max = 0;
+
+        let mut min_idx = 0;
+        let mut max_idx = 0;
+
+        for (i, &rank) in history.iter().enumerate() {
+            if rank < min {
+                min = rank;
+                min_idx = i;
+
+                if rank > max {
+                    max = rank;
+                    max_idx = i;
+                }
+            } else if rank > max {
+                max = rank;
+                max_idx = i;
+            }
+        }
+
+        let y_label_area_size = if max > 1_000_000 {
+            75
+        } else if max > 10_000 {
+            65
+        } else if max > 100 {
+            50
+        } else {
+            40
+        };
+
+        let (min, max) = (-(max as i32), -(min as i32));
+
+        {
+            let root = BitMapBackend::with_buffer(&mut buf, (W, H)).into_drawing_area();
+            let background = RGBColor(19, 43, 33);
+            root.fill(&background)?;
+
+            let circle_style: fn(RGBColor) -> ShapeStyle = |color| ShapeStyle {
+                color: color.to_rgba(),
+                filled: false,
+                stroke_width: 1,
+            };
+
+            let mut chart = ChartBuilder::on(&root)
+                .x_label_area_size(40)
+                .y_label_area_size(y_label_area_size)
+                .margin(10)
+                .margin_left(6)
+                .build_cartesian_2d(0_u32..history_len.saturating_sub(1) as u32, min..max)?;
+
+            chart
+                .configure_mesh()
+                .disable_y_mesh()
+                .x_labels(20)
+                .x_desc("Days ago")
+                .x_label_formatter(&|x| format!("{}", 90 - *x))
+                .y_label_formatter(&|y| format!("{}", -*y))
+                .y_desc("Rank")
+                .label_style(("sans-serif", 15, &WHITE))
+                .bold_line_style(&WHITE.mix(0.3))
+                .axis_style(RGBColor(7, 18, 14))
+                .axis_desc_style(("sans-serif", 16, FontStyle::Bold, &WHITE))
+                .draw()?;
+
+            let data = (0..).zip(history.iter().map(|rank| -(*rank as i32)));
+
+            let area_style = RGBColor(2, 186, 213).mix(0.8).filled();
+            let series = AreaSeries::new(data, min, area_style);
+            chart.draw_series(series)?;
+
+            let max_coords = (min_idx as u32, max);
+            let circle = Circle::new(max_coords, 9_u32, circle_style(GREEN));
+
+            chart
+                .draw_series(std::iter::once(circle))?
+                .label(format!("Peak: #{}", with_comma_int(-max)))
+                .legend(|(x, y)| Circle::new((x, y), 5_u32, circle_style(GREEN)));
+
+            let min_coords = (max_idx as u32, min);
+            let circle = Circle::new(min_coords, 9_u32, circle_style(RED));
+
+            chart
+                .draw_series(std::iter::once(circle))?
+                .label(format!("Worst: #{}", with_comma_int(-min)))
+                .legend(|(x, y)| Circle::new((x, y), 5_u32, circle_style(RED)));
+
+            let position = if min_idx <= 70 {
+                SeriesLabelPosition::UpperRight
+            } else if max_idx > 70 {
+                SeriesLabelPosition::UpperLeft
+            } else {
+                SeriesLabelPosition::LowerRight
+            };
+
+            chart
+                .configure_series_labels()
+                .border_style(BLACK.stroke_width(2))
+                .background_style(&RGBColor(192, 192, 192))
+                .position(position)
+                .legend_area_size(13)
+                .label_font(("sans-serif", 15, FontStyle::Bold))
+                .draw()?;
+        }
+
+        // Encode buf to png
+        let mut png_bytes: Vec<u8> = Vec::with_capacity(LEN);
+        let png_encoder = PngEncoder::new(&mut png_bytes);
+        png_encoder.write_image(&buf, W, H, ColorType::Rgb8)?;
+
+        Ok(Some(png_bytes))
+    }
+
+    let bytes = match draw_graph(&user) {
+        Ok(Some(graph)) => graph,
+        Ok(None) => {
+            let content = format!("`{name}` has no available rank data :(");
+            let _ = data.error(ctx, content).await?;
+
+            return Ok(None);
+        }
+        Err(err) => {
+            let _ = data.error(ctx, GENERAL_ISSUE).await;
+            warn!("{:?}", Report::new(err));
+
+            return Ok(None);
+        }
+    };
+
+    Ok(Some((user, bytes)))
 }
 
 async fn score_time_graph(
