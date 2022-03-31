4f477c2949238d3e9e3a00391ff5e24253bdf4bd
JoshiDhima <s0190681@ad.ua.ac.be>
2022-03-31 15:48:24 +0200

    add misscount, wait before game over to show pp

--- src/commands/fun/higherlower_game/higherlower.rs
+++ src/commands/fun/higherlower_game/higherlower.rs
@@ -1,4 +1,4 @@
-use std::fmt::Write;
+use std::{fmt::Write, time::Duration};
 
 use command_macros::SlashCommand;
 use dashmap::mapref::entry::Entry;
@@ -6,6 +6,7 @@ use eyre::Report;
 use image::{png::PngEncoder, ColorType, GenericImageView, ImageBuffer};
 use rand::Rng;
 use rosu_v2::prelude::{CountryCode, GameMode, GameMods, Grade, Username};
+use tokio::time::sleep;
 use twilight_interactions::command::CreateCommand;
 use twilight_model::{
     application::{
@@ -26,7 +27,7 @@ use crate::{
         constants::{GENERAL_ISSUE, HL_IMAGE_CHANNEL_ID, RED},
         numbers::{round, with_comma_int},
         osu::grade_emote,
-        ApplicationCommandExt, Authored, ChannelExt, ComponentExt,
+        ApplicationCommandExt, Authored, ChannelExt, ComponentExt, Emote,
     },
     BotResult, Context,
 };
@@ -205,7 +206,7 @@ async fn random_play(ctx: &Context, prev_pp: f32, curr_score: u32) -> BotResult<
         max_combo: map.max_combo.unwrap_or(0),
         score: play.score,
         acc: round(play.accuracy),
-        _miss_count: play.statistics.count_miss,
+        miss_count: play.statistics.count_miss,
         grade: play.grade,
         cover: format!(
             "https://assets.ppy.sh/beatmaps/{}/covers/cover.jpg",
@@ -331,7 +332,7 @@ struct HlGameStateInfo {
     max_combo: u32,
     score: u32,
     acc: f32,
-    _miss_count: u32,
+    miss_count: u32,
     grade: Grade,
     cover: String,
 }
@@ -354,7 +355,7 @@ impl HlGameStateInfo {
 
     fn play_string(&self, pp_visible: bool) -> String {
         format!(
-            "**{} {}**\n{} {} • **{}%** • **{}x**/{}x • **{}pp**",
+            "**{} {}**\n{} {} • **{}%** • **{}x**/{}x {}• **{}pp**",
             self.map_string,
             get_mods(self.mods),
             grade_emote(self.grade),
@@ -362,6 +363,11 @@ impl HlGameStateInfo {
             self.acc,
             self.combo,
             self.max_combo,
+            if self.miss_count > 0 {
+                format!("• **{}{}** ", self.miss_count, Emote::Miss.text())
+            } else {
+                String::new()
+            },
             if pp_visible {
                 self.pp.to_string().into()
             } else {
@@ -499,8 +505,15 @@ async fn correct_guess(
     }
 
     game.current_score += 1;
-    let image = game.create_image(&ctx).await?;
+    let image = match game.create_image(&ctx).await {
+        Ok(url) => url,
+        Err(err) => {
+            let report = Report::new(err).wrap_err("failed to create hl image");
+            warn!("{report:?}");
 
+            String::new()
+        }
+    };
     let embed = game.to_embed(image);
     let builder = MessageBuilder::new().embed(embed);
     component.update(&ctx, &builder).await?;
@@ -547,6 +560,7 @@ async fn game_over(
         .color(RED)
         .build();
 
+    sleep(Duration::from_secs(2)).await;
     let builder = MessageBuilder::new().embed(embed).components(Vec::new());
     component.update(&ctx, &builder).await?;
 
