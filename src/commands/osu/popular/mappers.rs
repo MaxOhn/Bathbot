ef31595c7a2f6f707f85b0bd034cacf9637ac50e
MaxOhn <ohn.m@hotmail.de>
2022-08-10 20:26:40 +0200

    truncate popular mappers before deserializing

--- src/commands/osu/popular/mappers.rs
+++ src/commands/osu/popular/mappers.rs
@@ -1,6 +1,6 @@
-use std::sync::Arc;
+use std::{alloc, mem, sync::Arc};
 
-use rkyv::{Deserialize, Infallible};
+use rkyv::{DeserializeUnsized, Infallible};
 use twilight_model::application::interaction::ApplicationCommand;
 
 use crate::{
@@ -12,12 +12,32 @@ use crate::{
 };
 
 pub(super) async fn mappers(ctx: Arc<Context>, command: Box<ApplicationCommand>) -> BotResult<()> {
-    let mut counts: Vec<OsuTrackerMapperEntry> = match ctx.redis().osutracker_stats().await {
-        Ok(stats) => stats
-            .get()
-            .mapper_count
-            .deserialize(&mut Infallible)
-            .unwrap(),
+    const LIMIT: usize = 500;
+
+    let counts: Vec<OsuTrackerMapperEntry> = match ctx.redis().osutracker_stats().await {
+        Ok(stats) => {
+            let counts = &stats.get().mapper_count;
+            let slice = &counts[..counts.len().min(LIMIT)];
+
+            unsafe {
+                // Deserialize to some location and get a pointer to it as *const ()
+                // i.e. a thin 8 byte pointer
+                let ptr =
+                    <[_] as DeserializeUnsized<[OsuTrackerMapperEntry], _>>::deserialize_unsized(
+                        slice,
+                        &mut Infallible,
+                        |layout| alloc::alloc(layout),
+                    )
+                    .unwrap();
+
+                // Transmute into a wide 16 byte pointer by appending the slice's metadata
+                // i.e. its length
+                let ptr = mem::transmute::<_, *mut [_]>((ptr, slice.len()));
+
+                // Construct a vec from the pointer
+                Box::<[_]>::from_raw(ptr).into()
+            }
+        }
         Err(err) => {
             let _ = command.error(&ctx, OSUTRACKER_ISSUE).await;
 
@@ -25,11 +45,8 @@ pub(super) async fn mappers(ctx: Arc<Context>, command: Box<ApplicationCommand>)
         }
     };
 
-    // TODO: only deserialize this many in the first place
-    counts.truncate(500);
-
     OsuTrackerMappersPagination::builder(counts)
-    .start_by_update()
+        .start_by_update()
         .start(ctx, CommandOrigin::Interaction { command })
         .await
 }
