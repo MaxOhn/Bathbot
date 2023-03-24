c6d8ec44bad385c2e01be07fb3c26676225ff214
MaxOhn <ohn.m@hotmail.de>
2023-03-24 17:39:28 +0100

    fix(bot): flip pb condition order

--- bathbot/src/util/osu.rs
+++ bathbot/src/util/osu.rs
@@ -651,7 +651,7 @@ impl PersonalBestIndex {
             return Self::NotTop100;
         } else if !matches!(status, RankStatus::Ranked | RankStatus::Approved) {
             return Self::IfRanked { idx };
-        } else if score.is_eq(&top100[idx]) || top100.len() < 100 {
+        } else if top100.len() < 100 || score.is_eq(&top100[idx]) {
             // If multiple scores have the exact same pp as the given
             // score then `idx` might not belong to the given score.
             // Chances are pretty slim though so this should be fine.
