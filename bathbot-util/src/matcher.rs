7869be504b2185ab7cdc205740044e30ae0e0fef
MaxOhn <ohn.m@hotmail.de>
2023-05-09 13:05:52 +0200

    feat(util): add github.com as approved skin sites

--- bathbot-util/src/matcher.rs
+++ bathbot-util/src/matcher.rs
@@ -204,7 +204,7 @@ define_regex! {
 
     OSU_SCORE_URL_MATCHER: r"https://osu.ppy.sh/scores/(osu|taiko|mania|fruits)/(\d+)";
 
-    APPROVED_SKIN_SITE: r"^https://(?:www\.)?(?:(?:drive\.google\.com|dropbox\.com|mega\.nz|mediafire\.com)/.*$|skins\.osuck\.net/skins/\d+.*)";
+    APPROVED_SKIN_SITE: r"^https://(?:www\.)?(?:(?:drive\.google\.com|dropbox\.com|mega\.nz|mediafire\.com|github\.com)/.*$|skins\.osuck\.net/skins/\d+.*)";
 }
 
 pub static QUERY_SYNTAX_REGEX: Regex =
