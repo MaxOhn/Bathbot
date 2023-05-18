b9740b4906366eb2f53373403a73bd348f109633
respektive <liambzw@gmail.com>
2023-05-18 12:24:21 +0200

    add skin tooltip to skin list

--- bathbot/src/embeds/utility/skins.rs
+++ bathbot/src/embeds/utility/skins.rs
@@ -2,7 +2,7 @@ use std::fmt::Write;
 
 use bathbot_macros::EmbedData;
 use bathbot_psql::model::configs::SkinEntry;
-use bathbot_util::{constants::OSU_BASE, FooterBuilder};
+use bathbot_util::{constants::OSU_BASE, CowUtils, FooterBuilder};
 
 use crate::pagination::Pages;
 
@@ -40,24 +40,26 @@ impl SkinsEmbed {
         for ((left, right), idx) in user_iter.zip(idx + 1..) {
             let _ = write!(
                 description,
-                "`#{idx:<idx_len$}` [`{name:<name_len$}`]({OSU_BASE}u/{user_id}) [`Skin`]({skin_url})",
+                "`#{idx:<idx_len$}` [`{name:<name_len$}`]({OSU_BASE}u/{user_id}) [`Skin`]({skin_url} \"{skin_tooltip}\")",
                 idx_len = left_lengths.idx,
                 name = left.username,
                 name_len = left_lengths.name,
                 user_id = left.user_id,
                 skin_url = left.skin_url,
+                skin_tooltip = left.skin_url.cow_replace("https://", ""),
             );
 
             if let Some(right) = right {
                 let _ = write!(
                     description,
-                    "|`#{idx:<idx_len$}` [`{name:<name_len$}`]({OSU_BASE}u/{user_id}) [`Skin`]({skin_url})",
+                    "|`#{idx:<idx_len$}` [`{name:<name_len$}`]({OSU_BASE}u/{user_id}) [`Skin`]({skin_url} \"{skin_tooltip}\")",
                     idx = idx + 10,
                     idx_len = right_lengths.idx,
                     name = right.username,
                     name_len = right_lengths.name,
                     user_id = right.user_id,
                     skin_url = right.skin_url,
+                    skin_tooltip = right.skin_url.cow_replace("https://", ""),
                 );
             }
 
