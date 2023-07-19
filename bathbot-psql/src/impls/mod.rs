33c3210c14d996d958d5a366220d603a34ad3156
MaxOhn <ohn.m@hotmail.de>
2023-07-19 16:23:12 +0200

    test(psql): remove db tests

--- bathbot-psql/src/impls/mod.rs
+++ bathbot-psql/src/impls/mod.rs
@@ -3,12 +3,3 @@ mod configs;
 mod games;
 mod osu;
 mod tracked_streams;
-
-#[cfg(test)]
-mod tests {
-    pub use super::{
-        configs::guild::tests::wrap_upsert_delete as guild_config_wrap_upsert_delete,
-        configs::user::tests::wrap_upsert_delete as user_config_wrap_upsert_delete,
-        osu::name::tests::wrap_upsert_delete as osu_name_wrap_upsert_delete,
-    };
-}
