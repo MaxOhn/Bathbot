97cdc2a2e4180e02d91ff3f4c240d96d476bee90
respektive <respektivee@gmail.com>
2023-10-05 16:33:11 +0200

    add peak score rank to respektive api

--- bathbot-model/src/respektive.rs
+++ bathbot-model/src/respektive.rs
@@ -1,13 +1,23 @@
 use std::vec::IntoIter;
 
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
+use time::OffsetDateTime;
 
-#[derive(Deserialize, Serialize)]
+use crate::deser::datetime_rfc3339;
+
+#[derive(Deserialize)]
+pub struct RespektiveUserRankHighest {
+    pub rank: u32,
+    #[serde(with = "datetime_rfc3339")]
+    pub updated_at: OffsetDateTime,
+}
+#[derive(Deserialize)]
 pub struct RespektiveUser {
     pub rank: u32,
     pub user_id: u32,
     #[serde(rename = "score")]
     pub ranked_score: u64,
+    pub rank_highest: Option<RespektiveUserRankHighest>,
 }
 
 pub struct RespektiveUsers {
