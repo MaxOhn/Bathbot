7c14932bb8add405f897fb9232a21142d5501737
MaxOhn <ohn.m@hotmail.de>
2023-10-09 13:30:36 +0200

    chore(bot): added warmups count logging

--- bathbot/src/commands/osu/match_costs.rs
+++ bathbot/src/commands/osu/match_costs.rs
@@ -150,6 +150,8 @@ async fn matchcosts(ctx: Arc<Context>, orig: CommandOrigin<'_>, args: MatchCost<
         }
     };
 
+    debug!("Match cost warmups: {warmups:?}");
+
     let warmups = warmups.unwrap_or(2);
     let ez_mult = ez_mult.unwrap_or(1.0);
     let skip_last = skip_last.unwrap_or(0);
