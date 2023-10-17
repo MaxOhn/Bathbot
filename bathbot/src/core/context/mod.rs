7971cf0ec4c505ce31a3dd53dc5fd9f83a3ed884
MaxOhn <ohn.m@hotmail.de>
2023-10-18 00:43:16 +0200

    feat: include buckets for histograms

--- bathbot/src/core/context/mod.rs
+++ bathbot/src/core/context/mod.rs
@@ -116,7 +116,14 @@ impl Context {
 
     pub async fn new(tx: UnboundedSender<(Id<GuildMarker>, u64)>) -> Result<ContextTuple> {
         let (_prometheus, reader) = {
-            let prometheus = metrics_exporter_prometheus::PrometheusBuilder::new().build_recorder();
+            const DEFAULT_BUCKETS: [f64; 10] =
+                [0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0];
+
+            let prometheus = metrics_exporter_prometheus::PrometheusBuilder::new()
+                .set_buckets(&DEFAULT_BUCKETS)
+                .expect("DEFAULT_BUCKETS is not empty")
+                .build_recorder();
+
             let prometheus_handle = prometheus.handle();
 
             let reader = MetricsReader::new();
