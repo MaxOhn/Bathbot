7dba22de2c4997fc01d05058a6978f052aba2757
MaxOhn <ohn.m@hotmail.de>
2023-11-17 12:33:17 +0100

    fix: add index to user_scores migration

--- bathbot-psql/migrations/20231020095331_materialized_scores_view.up.sql
+++ bathbot-psql/migrations/20231020095331_materialized_scores_view.up.sql
@@ -8,6 +8,7 @@ FROM
         JOIN osu_user_stats USING (user_id)
         JOIN osu_scores_performance USING (score_id);
 
+CREATE UNIQUE INDEX user_scores_score_id_index ON user_scores (score_id);
 CREATE INDEX user_scores_mode_pp_index ON user_scores (gamemode, pp DESC);
 CREATE INDEX user_scores_mode_country_user_index ON user_scores (gamemode, country_code, user_id);
 CREATE INDEX user_scores_mode_country_pp_index ON user_scores (gamemode, country_code, pp DESC);
