9e60ae68f576494ac8a8bb148722b092ca11ce7e
MaxOhn <ohn.m@hotmail.de>
2024-07-12 20:58:21 +0200

    dep: remove itertools

--- bathbot/src/embeds/tracking/list.rs
+++ bathbot/src/embeds/tracking/list.rs
@@ -2,7 +2,6 @@ use std::fmt::Write;
 
 use bathbot_macros::EmbedData;
 use bathbot_util::constants::DESCRIPTION_SIZE;
-use itertools::Itertools;
 use rosu_v2::model::GameMode;
 
 use crate::commands::tracking::TracklistUserEntry;
@@ -15,26 +14,56 @@ pub struct TrackListEmbed {
 
 impl TrackListEmbed {
     pub fn new(users: Vec<TracklistUserEntry>) -> Vec<Self> {
+        const MODES: [GameMode; 4] = [
+            GameMode::Osu,
+            GameMode::Taiko,
+            GameMode::Catch,
+            GameMode::Mania,
+        ];
+
         let mut embeds = Vec::new();
         let title = "Tracked osu! users in this channel (limit)";
         let mut description = String::with_capacity(256);
 
-        users
-            .into_iter()
-            .group_by(|entry| entry.mode)
-            .into_iter()
-            .for_each(|(mode, group)| {
-                let mode = match mode {
-                    GameMode::Osu => "osu!standard",
-                    GameMode::Mania => "osu!mania",
-                    GameMode::Taiko => "osu!taiko",
-                    GameMode::Catch => "osu!ctb",
-                };
+        // One group per mode
+        let mut groups = [(); 4].map(|_| Vec::new());
 
-                description.reserve(256);
-                let mut names = group.map(|entry| (entry.name, entry.limit));
-                let (first_name, first_limit) = names.next().unwrap();
-                let len = description.chars().count() + mode.len() + first_name.chars().count() + 7;
+        for entry in users {
+            groups[entry.mode as usize].push(entry);
+        }
+
+        for (mode, group) in MODES.into_iter().zip(groups) {
+            let mut names = group.into_iter().map(|entry| (entry.name, entry.limit));
+
+            let Some((first_name, first_limit)) = names.next() else {
+                continue;
+            };
+
+            let mode = match mode {
+                GameMode::Osu => "osu!standard",
+                GameMode::Mania => "osu!mania",
+                GameMode::Taiko => "osu!taiko",
+                GameMode::Catch => "osu!ctb",
+            };
+
+            description.reserve(256);
+            let len = description.chars().count() + mode.len() + first_name.chars().count() + 7;
+
+            if len > DESCRIPTION_SIZE {
+                embeds.push(Self {
+                    title,
+                    description: description.to_owned(),
+                });
+
+                description.clear();
+            }
+
+            let _ = writeln!(description, "__**{mode}**__");
+            let _ = write!(description, "`{first_name}` ({first_limit})");
+            let mut with_comma = true;
+
+            for (name, limit) in names {
+                let len = description.chars().count() + name.chars().count() + 9;
 
                 if len > DESCRIPTION_SIZE {
                     embeds.push(Self {
@@ -43,37 +72,21 @@ impl TrackListEmbed {
                     });
 
                     description.clear();
+                    let _ = writeln!(description, "__**{mode}**__");
+                    with_comma = false;
                 }
 
-                let _ = writeln!(description, "__**{mode}**__");
-                let _ = write!(description, "`{first_name}` ({first_limit})");
-                let mut with_comma = true;
+                let _ = write!(
+                    description,
+                    "{}`{name}` ({limit})",
+                    if with_comma { ", " } else { "" },
+                );
 
-                for (name, limit) in names {
-                    let len = description.chars().count() + name.chars().count() + 9;
+                with_comma = true;
+            }
 
-                    if len > DESCRIPTION_SIZE {
-                        embeds.push(Self {
-                            title,
-                            description: description.to_owned(),
-                        });
-
-                        description.clear();
-                        let _ = writeln!(description, "__**{mode}**__");
-                        with_comma = false;
-                    }
-
-                    let _ = write!(
-                        description,
-                        "{}`{name}` ({limit})",
-                        if with_comma { ", " } else { "" },
-                    );
-
-                    with_comma = true;
-                }
-
-                description.push('\n');
-            });
+            description.push('\n');
+        }
 
         if description.lines().count() > 1 {
             embeds.push(Self { description, title });
