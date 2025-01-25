276e473040f539229f27064bcca34db689e131e1
MaxOhn <ohn.m@hotmail.de>
2025-01-25 16:38:34 +0100

    feat: show missing score for levelup in profile

--- bathbot-util/src/osu.rs
+++ bathbot-util/src/osu.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp,
     iter::{self, Copied, Map},
     slice::Iter,
 };
@@ -9,6 +10,164 @@ use rosu_v2::prelude::{
 };
 
 use crate::{constants::OSU_BASE, numbers::round};
+
+// <https://github.com/ppy/osu-queue-score-statistics/blob/45cd68bb1ec974ee433a9cb649e412a3376b130e/osu.Server.Queues.ScoreStatisticsProcessor/Processors/TotalScoreProcessor.cs#L91-L116>
+const TO_NEXT_LEVEL: [u64; 123] = [
+    30000,
+    100000,
+    210000,
+    360000,
+    550000,
+    780000,
+    1050000,
+    1360000,
+    1710000,
+    2100000,
+    2530000,
+    3000000,
+    3510000,
+    4060000,
+    4650000,
+    5280000,
+    5950000,
+    6660000,
+    7410000,
+    8200000,
+    9030000,
+    9900000,
+    10810000,
+    11760000,
+    12750000,
+    13780000,
+    14850000,
+    15960000,
+    17110000,
+    18300000,
+    19530000,
+    20800000,
+    22110000,
+    23460000,
+    24850000,
+    26280000,
+    27750000,
+    29260000,
+    30810000,
+    32400000,
+    34030000,
+    35700000,
+    37410000,
+    39160000,
+    40950000,
+    42780000,
+    44650000,
+    46560000,
+    48510000,
+    50500000,
+    52530000,
+    54600000,
+    56710000,
+    58860000,
+    61050000,
+    63280000,
+    65550000,
+    67860000,
+    70210001,
+    72600001,
+    75030002,
+    77500003,
+    80010006,
+    82560010,
+    85150019,
+    87780034,
+    90450061,
+    93160110,
+    95910198,
+    98700357,
+    101530643,
+    104401157,
+    107312082,
+    110263748,
+    113256747,
+    116292144,
+    119371859,
+    122499346,
+    125680824,
+    128927482,
+    132259468,
+    135713043,
+    139353477,
+    143298259,
+    147758866,
+    153115959,
+    160054726,
+    169808506,
+    184597311,
+    208417160,
+    248460887,
+    317675597,
+    439366075,
+    655480935,
+    1041527682,
+    1733419828,
+    2975801691,
+    5209033044,
+    9225761479,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+    99999999999,
+];
+
+pub fn total_score_to_reach_level(target_level: u32) -> u64 {
+    TO_NEXT_LEVEL
+        .into_iter()
+        .chain(iter::repeat(TO_NEXT_LEVEL[TO_NEXT_LEVEL.len() - 1]))
+        .take(target_level.saturating_sub(1) as usize)
+        .sum()
+}
+
+// <https://github.com/ppy/osu-queue-score-statistics/blob/45cd68bb1ec974ee433a9cb649e412a3376b130e/osu.Server.Queues.ScoreStatisticsProcessor/Processors/TotalScoreProcessor.cs#L70-L89>
+#[allow(unused)]
+fn calculate_level(total_score: u64) -> f32 {
+    let mut remaining_score = total_score as i64;
+    let mut level: f32 = 0.0;
+
+    while remaining_score > 0 {
+        // * if we're exceeding available array entries, continue using the requirement
+        // * for the highest level.
+        let next_level_requirement =
+            TO_NEXT_LEVEL[cmp::min(TO_NEXT_LEVEL.len() - 1, level.round() as usize)];
+
+        // * always increment by at most one level, but include the fractional portion
+        // * for the final level.
+        level += (remaining_score as f32 / next_level_requirement as f32).min(1.0);
+
+        remaining_score -= next_level_requirement as i64;
+    }
+
+    // * were using level as a zero based index, but storage is one-based.
+    level + 1.0
+}
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum ModSelection {
