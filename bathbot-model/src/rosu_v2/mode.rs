c9cc26e57f333a13a5203f3c44d21b23cc5b436b
MaxOhn <ohn.m@hotmail.de>
2025-02-20 17:58:36 +0100

    fix: handle pointer properly

--- bathbot-model/src/rosu_v2/mode.rs
+++ bathbot-model/src/rosu_v2/mode.rs
@@ -1,4 +1,4 @@
-use rkyv::{niche::niching::Niching, Archive, Place};
+use rkyv::{niche::niching::Niching, Place};
 use rosu_mods::GameMode;
 
 pub struct GameModeNiche;
@@ -9,10 +9,10 @@ impl GameModeNiche {
 
 impl Niching<GameMode> for GameModeNiche {
     unsafe fn is_niched(niched: *const GameMode) -> bool {
-        unsafe { *niched as u8 == Self::NICHED }
+        unsafe { *niched.cast::<u8>() == Self::NICHED }
     }
 
     fn resolve_niched(out: Place<GameMode>) {
-        Self::NICHED.resolve((), unsafe { out.cast_unchecked() });
+        unsafe { out.cast_unchecked::<u8>() }.write(Self::NICHED);
     }
 }
